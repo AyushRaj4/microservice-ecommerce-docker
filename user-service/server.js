dd6570e73620ecefc100e3e5412bb004703e064f
Ayush Raj <ayushraj2901@gmail.com>
2025-09-10 23:44:27 +0530

    Added retries for connecting rabbitMQ, connected order service, product service to db and remaining scripts

--- user-service/server.js
+++ user-service/server.js
@@ -1,18 +1,27 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-const {PORT} = require('./utils/config');
-const connectDB = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
+const { PORT } = require("./utils/config");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const { connectRabbitMQ, consumeMessage } = require("./utils/rabbitMQ");
 
 connectDB();
 
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
+async function notifyUser(order) {
+  console.log("User service received order:", order);
+}
 
+app.use("/api/auth", authRoutes);
+
+(async () => {
+  await connectRabbitMQ();
+  await consumeMessage("orderQueue", notifyUser);
+})();
 
 app.listen(PORT, () => {
   console.log(`User service is running on port ${PORT}`);
